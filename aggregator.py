8053bd4006353d12f3522846d86ba5b203eb616b
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-12 21:23:27 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -1,19 +1,8 @@
-from aliexpress_client import ali_search
-
 def get_feed():
-    try:
-        items = ali_search("women clothes", limit=20)
-    except Exception:
-        items = []
-
-    if not items:
-        return [{
-            "id": "error",
-            "title": "Нет данных",
-            "price": 0,
-            "image": "https://i.imgur.com/0ZfQZQh.jpeg",
-            "source": "system"
-        }]
-
-    items.sort(key=lambda x: x["price"])
-    return items
+    return [{
+        "id": "test",
+        "title": "Тестовый товар",
+        "price": 123,
+        "image": "https://i.imgur.com/0ZfQZQh.jpeg",
+        "source": "test"
+    }]
