3a27eaa8052c680bc91a504c1ae2092b6e01932e
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-12 13:50:55 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+﻿from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Mini App is working!"
+
+@app.route("/api/items")
+def items():
+    return jsonify([
+        {"id": 1, "name": "Товар 1", "price": 100},
+        {"id": 2, "name": "Товар 2", "price": 200},
+        {"id": 3, "name": "Товар 3", "price": 300}
+    ])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
