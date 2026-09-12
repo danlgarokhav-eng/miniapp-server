94e426c7b0dbfc28fdca004917f2b7c55376c47a
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-12 20:47:14 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -2,16 +2,11 @@ from wb_client import wb_search
 from ozon_client import ozon_category
 
 def get_feed():
-    # WB: устойчивый поиск
     wb_items = wb_search("платье", limit=10)
-
-    # Ozon: публичная категория
     ozon_items = ozon_category("/category/smartfony-15542/", limit=10)
 
-    # Объединяем
     feed = wb_items + ozon_items
 
-    # Если WB заблокировал и Ozon ничего не дал — не ломаем сайт
     if not feed:
         return [{
             "id": "error",
@@ -21,7 +16,5 @@ def get_feed():
             "source": "system"
         }]
 
-    # Сортировка по цене
     feed.sort(key=lambda x: x["price"])
-
     return feed
