fe6beb4dc79640663fd464699c8e4777c8c84905
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-12 21:19:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify
 from aggregator import get_feed
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
 @app.route("/api/feed")
 def feed():
     return jsonify(get_feed())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
