1c99c5a70590f73fb6cf3f8ad4ac119e8bb16f4f
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-13 22:29:09 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -2,6 +2,7 @@ from parser_wb import parse_wb
 import json
 
 def generate_feed():
+    # Можешь менять запрос на любой: "кроссовки", "телефон", "рюкзак"
     wb_items = parse_wb("платье", limit=10)
 
     with open("feed.json", "w", encoding="utf-8") as f:
