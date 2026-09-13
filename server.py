104b4fc32b4d9b39b437a8a755a42daec3c8a553
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-13 19:25:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+from flask import Flask, jsonify
+from aggregator import get_feed
 
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "SERVER OK"
+
+@app.route("/api/feed")
+def feed():
+    return jsonify(get_feed())
