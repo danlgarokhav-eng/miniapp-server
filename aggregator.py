518d1f27f8cff6ac376640ac40463b34aa513a5d
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-13 23:02:25 +0300

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -1,14 +1,5 @@
 from parser_wb import parse_wb
-import json
 
 def generate_feed():
-    wb_items = parse_wb("платье", limit=20)
-
-    if not wb_items:
-        print("WB не дал товары, feed.json не обновлён")
-        return
-
-    with open("feed.json", "w", encoding="utf-8") as f:
-        json.dump(wb_items, f, ensure_ascii=False, indent=2)
-
-    print("feed.json обновлён!")
+    parse_wb()
+    print("Парсер завершил работу")
