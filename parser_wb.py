37171916d455c4f631f1c7a9c21cb0b053d40fd3
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-13 22:19:04 +0300

    Update parser_wb.py

--- parser_wb.py
+++ parser_wb.py
@@ -1,5 +1,21 @@
-def parse_wb():
-    return [
-        {"title": "Тестовый товар 1", "price": "1000 ₽"},
-        {"title": "Тестовый товар 2", "price": "2000 ₽"},
-    ]
+import requests
+import json
+
+def parse_wb(query="платье", limit=20):
+    url = f"https://catalog.wb.ru/catalog/electronic/v4/search?appType=1&curr=rub&dest=-1257786&query={query}"
+    r = requests.get(url)
+    data = r.json()
+
+    products = []
+
+    for item in data.get("data", {}).get("products", []):
+        products.append({
+            "title": item.get("name", "Без названия"),
+            "price": f"{item.get('salePriceU', 0) // 100} ₽",
+            "image": f"https://images.wbstatic.net/c246x328/new/{item['id']}-1.jpg"
+        })
+
+        if len(products) >= limit:
+            break
+
+    return products
