3e1551489fc7b7e63ed01fea7b747bcb94246891
danlgarokhav-eng <danl.garokhav@mail.ru>
2026-09-13 23:02:11 +0300

    Update parser_wb.py

--- parser_wb.py
+++ parser_wb.py
@@ -1,31 +1,9 @@
-import requests
+from playwright.sync_api import sync_playwright
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-}
-
-def parse_wb(query="платье", limit=20):
-    url = (
-        "https://search.wb.ru/exactmatch/ru/common/v4/search"
-        f"?query={query}&resultset=catalog"
-        "&sort=popular&curr=rub&lang=ru"
-    )
-
-    r = requests.get(url, headers=HEADERS)
-
-    try:
-        data = r.json()
-    except:
-        print("WB вернул НЕ JSON")
-        return []
-
-    products = []
-
-    for item in data.get("data", {}).get("products", [])[:limit]:
-        products.append({
-            "title": item.get("name", "Без названия"),
-            "price": f"{item.get('salePriceU', item.get('priceU', 0)) // 100} ₽",
-            "image": f"https://images.wbstatic.net/c246x328/new/{item['id']}-1.jpg"
-        })
-
-    return products
+def parse_wb():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        print("Открыл Google!")
+        browser.close()
